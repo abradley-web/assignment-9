d75c2f80cd9500402ae16a34f3f18a2deb30f8c5
abradley-web <abradley4005@gmail.com>
2025-11-02 18:28:25 -0600

    Used aync/await to fetch gifs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("script.js loaded");
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=Tpy6v24UnzXgXB4XZ21G1ZwvjBEhXd2D&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+async function fetchGifs() {
+  try {
+    // Fetch data from the Giphy API
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    // Extract the original image URLs and store in an array
+    const images = data.data.map(gif => gif.images.original.url);
+
+    // Preview the data in the console
+    console.log(images);
+      } catch (error) {
+    console.error("Error fetching GIFs:", error);
+  }
+}
